9f909b39b39ac719dac21cf6e1b1c6a4a4a57349
halexan7 <halexan7@msudenver.edu>
2022-04-25 11:03:56 -0600

    My first commit for this project

---
+++ src/SourceCode.java
@@ -0,0 +1,27 @@
+//The program takes a "C program Source Code"
+//reads it into a list and counts the number of comment lines
+//return an output telling the user how many lines of comments are present
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.io.File;
+
+public class SourceCode {
+    public static void main(String[] args) throws IOException {
+        int count = 0;
+//        File file = new File("C:\\Users\\hardw\\IdeaProjects\\Assignment2\\src\\example1.txt");
+//        Scanner input = new Scanner(file);
+        List<String> list = Files.readAllLines(Path.of("C:\\Users\\hardw\\IdeaProjects\\" +
+                "Assignment2\\src\\example1.txt"), Charset.defaultCharset());
+        for (int i = 0; i < list.size(); i++){
+            if (list.get(i).contains("//")){
+                count++;
+            }
+        }
+        System.out.println("You're source code has " + count + " lines of comments");
+    }
+}
